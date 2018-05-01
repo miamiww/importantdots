fb58c8f1b4fb5ab56110e537c3899c02212edcfc
miamiworldwide <rivendalejones@gmail.com>
2018-04-30 20:48:51 -0400

    switching to config method

---
+++ .aliases.sh
@@ -0,0 +1,15 @@
+echo 'setting aliases'
+
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+
+export PS1="👻 🌵 ✨ $ "
+
+alias chromeTest='open -a "Google Chrome" "http://localhost:3000"'
+alias questionRead='cat /Users/aljones/ITP/Year1/Semester2/Detourning/2Week/assignment/yahooQuestions.txt | say -v Fred '
+alias gs='git status'
+alias deployH='bash ~/ITP/miamiww.github.io/deploy.sh'
+alias slidebot800='/Users/aljones/ITP/Year1/Semester2/ElectronicText/final_project/slidebot'
+alias ls='ls -GFh'
+alias screen="/usr/local/bin/screen"
---
+++ .path_setup.sh
@@ -0,0 +1,10 @@
+echo "setting paths"
+
+# homebrew
+export PATH="/usr/local/bin:$PATH"
+
+#added by Anaconda3 4.1.0 installer
+export PATH="//Users/aljones/anaconda/bin:$PATH"
+
+#trying to tell homebrew where xcode is
+export PATH="/Applications/Xcode.app/Contents/Developer:$PATH"
