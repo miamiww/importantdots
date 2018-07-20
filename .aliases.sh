acdc0a062694c08df5d4d36ddbd2ffb6b53b7dbb
miamiworldwide <rivendalejones@gmail.com>
2018-07-20 20:06:10 +0900

    fixing up tmux for vim

--- .aliases.sh
+++ .aliases.sh
@@ -1,6 +1,6 @@
 echo 'setting aliases'
 
-alias tmux="TERM=screen-256color-bce tmux"
+#alias tmux="TERM=screen-256color-bce tmux"
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
