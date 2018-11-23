6734f23f1bd63fecf17fba3a7f715b1e3efd7ce8
miamiww <rivendalejones@gmail.com>
2018-11-22 23:21:40 -0500

    I hope I don't regret this

--- .path_setup.sh
+++ .path_setup.sh
@@ -4,7 +4,7 @@ echo "setting paths"
 export PATH="/usr/local/bin:$PATH"
 
 #added by Anaconda3 4.1.0 installer
-export PATH="//Users/aljones/anaconda/bin:$PATH"
+export PATH="/Users/aljones/anaconda/bin:$PATH"
 
 #trying to tell homebrew where xcode is
 export PATH="/Applications/Xcode.app/Contents/Developer:$PATH"
