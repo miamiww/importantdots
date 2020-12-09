4e7700103c9b87feddd27033eac8d2ef723f6f20
Alden Jones <rivendalejones@gmail.com>
2020-12-09 14:45:43 -0500

    adding additional tools for btd

--- .btd_aliases.sh
+++ .btd_aliases.sh
@@ -3,3 +3,9 @@ echo "loading btd commands"
 alias btddb_status='heroku pg:info -a big-tech-detective-api'
 alias btddb='heroku pg:psql -a big-tech-detective-api'
 alias btd_checklogs='heroku logs --tail'
+alias btd_updatedb='cat ip_ranges_asn_only.csv | psql `heroku config:get DATABASE_URL -a big-tech-detective-api` -c "COPY test FROM STDIN WITH (FORMAT CSV);"'
+
+alias btd_local_db='psql -d postgres -U alden'
+
+export DATABASE_URL="postgresql://alden:antitrustviolations@localhost:5432/cidr_blocks"
+export PORT="8080"
